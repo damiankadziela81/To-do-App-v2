3ba9ea49cceaa14d89a68a0913244068ac0caa6a
damiankadziela81 <damiankadziela02@gmail.com>
2023-02-21 13:26:51 +0100

    introduced public TaskRepository as to narrow available methods for the controller

---
+++ src/main/java/com/example/model/SqlTaskRepository.java
@@ -0,0 +1,9 @@
+package com.example.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface SqlTaskRepository extends TaskRepository, JpaRepository<Task,Integer> {
+
+}
--- src/main/java/com/example/model/TaskRepository.java
+++ src/main/java/com/example/model/TaskRepository.java
@@ -1,14 +1,20 @@
 package com.example.model;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
-@Repository
-public interface TaskRepository extends JpaRepository<Task,Integer> {
+public interface TaskRepository {
 
+    List<Task> findAll();
+
+    Page<Task> findAll(Pageable page);
+    
+    Optional<Task> findById(Integer id);
+    Task save(Task entity);
 
     List<Task> findByDone(@Param("state") boolean done);
 
