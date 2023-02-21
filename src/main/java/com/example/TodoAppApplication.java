5ec6f5940346a2cf48349ce57fce3cd840c6c527
damiankadziela81 <damiankadziela02@gmail.com>
2023-02-21 12:37:23 +0100

    transition from RestRepository to fully custom controller

--- src/main/java/com/example/TodoAppApplication.java
+++ src/main/java/com/example/TodoAppApplication.java
@@ -1,28 +1,21 @@
 package com.example;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
-public class TodoAppApplication implements RepositoryRestConfigurer {
+public class TodoAppApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoAppApplication.class, args);
 	}
 
-	@Override
-	public void configureValidatingRepositoryEventListener(final ValidatingRepositoryEventListener validatingListener) {
-		validatingListener.addValidator("beforeCreate", validator());
-		validatingListener.addValidator("beforeSave",validator());
-
-	}
 	@Bean
-	Validator validator() {
+	LocalValidatorFactoryBean validator(){
 		return new LocalValidatorFactoryBean();
 	}
+
 }
