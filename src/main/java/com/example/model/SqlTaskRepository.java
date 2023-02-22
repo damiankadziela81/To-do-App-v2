5341fbc6981009f6363a18b6ad9d16d7d974a598
damiankadziela81 <damiankadziela02@gmail.com>
2023-02-22 14:20:47 +0100

    native querry practice

--- src/main/java/com/example/model/SqlTaskRepository.java
+++ src/main/java/com/example/model/SqlTaskRepository.java
@@ -1,9 +1,15 @@
 package com.example.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface SqlTaskRepository extends TaskRepository, JpaRepository<Task,Integer> {
-
+    @Override
+//    @Query(nativeQuery = true, value = "select count(*) > 0 from tasks where id=?1")
+//    boolean existsById(Integer id);
+    @Query(nativeQuery = true, value = "select count(*) > 0 from tasks where id=:id")
+    boolean existsById(@Param("id") Integer id);
 }
