3f18e21691898a315156099e71f311b56a35678d
damiankadziela81 <damiankadziela02@gmail.com>
2023-02-23 14:05:12 +0100

    Created interface and implementation for TaskGroupRepository

---
+++ src/main/java/com/example/model/TaskGroupRepository.java
@@ -0,0 +1,10 @@
+package com.example.model;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskGroupRepository {
+    List<TaskGroup> findAll();
+    Optional<TaskGroup> findById(Integer id);
+    TaskGroup save(TaskGroup entity);
+}
--- src/main/java/com/example/model/TaskRepository.java
+++ src/main/java/com/example/model/TaskRepository.java
@@ -19,6 +19,8 @@ public interface TaskRepository {
 
     Task save(Task entity);
 
+    boolean existByDoneIsFalseAndGroup_Id(Integer groupId);
+
     List<Task> findByDone(boolean done);
 
 
