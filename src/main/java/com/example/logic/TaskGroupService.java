7456dde24d32cb410679b3c331d0ea853b90f837
damiankadziela81 <damiankadziela02@gmail.com>
2023-02-24 13:06:44 +0100

    Added service layer

---
+++ src/main/java/com/example/logic/TaskGroupService.java
@@ -0,0 +1,46 @@
+package com.example.logic;
+
+import com.example.TaskConfiguration;
+import com.example.model.TaskGroup;
+import com.example.model.TaskGroupRepository;
+import com.example.model.TaskRepository;
+import com.example.model.projection.GroupReadModel;
+import com.example.model.projection.GroupWriteModel;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequestScope
+public class TaskGroupService {
+    private TaskGroupRepository repository;
+    private TaskRepository taskRepository;
+
+    TaskGroupService(final TaskGroupRepository repository, final TaskRepository taskRepository) {
+        this.repository = repository;
+        this.taskRepository = taskRepository;
+    }
+
+    public GroupReadModel createGroup(GroupWriteModel source) {
+        TaskGroup result = repository.save(source.toGroup());
+        return new GroupReadModel(result);
+    }
+
+    public List<GroupReadModel> readAll() {
+        return repository.findAll().stream()
+                .map(GroupReadModel::new)
+                .collect(Collectors.toList());
+    }
+
+    public void toggleGroup(int groupId) {
+        if(taskRepository.existsByDoneIsFalseAndGroup_Id(groupId)) {
+            throw new IllegalStateException("Group still has pending tasks. Finish all the tasks first! ");
+        }
+        TaskGroup result = repository.findById(groupId)
+                .orElseThrow(() -> new IllegalArgumentException("TaskGroup with given id not found"));
+        result.setDone(!result.isDone());
+    }
+
+}
