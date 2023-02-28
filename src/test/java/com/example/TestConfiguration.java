5b6eaa09c6afdee22142e1211f089388b06f883a
damiankadziela81 <damiankadziela02@gmail.com>
2023-02-28 13:47:04 +0100

    Added test configuration with in-memory task repository

---
+++ src/test/java/com/example/TestConfiguration.java
@@ -0,0 +1,59 @@
+package com.example;
+
+import com.example.model.Task;
+import com.example.model.TaskRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.*;
+
+@Configuration
+class TestConfiguration {
+
+    @Bean
+    @Profile("integration")
+    TaskRepository testRepo() {
+        return new TaskRepository() {
+
+            private Map<Integer, Task> tasks = new HashMap<>();
+
+            @Override
+            public List<Task> findAll() {
+                return new ArrayList<>(tasks.values());
+            }
+
+            @Override
+            public Page<Task> findAll(final Pageable page) {
+                return null;
+            }
+
+            @Override
+            public Optional<Task> findById(final Integer id) {
+                return Optional.ofNullable(tasks.get(id));
+            }
+
+            @Override
+            public boolean existsById(final Integer id) {
+                return tasks.containsKey(id);
+            }
+
+            @Override
+            public Task save(final Task entity) {
+                return tasks.put(tasks.size() + 1, entity);
+            }
+
+            @Override
+            public boolean existsByDoneIsFalseAndGroup_Id(final Integer groupId) {
+                return false;
+            }
+
+            @Override
+            public List<Task> findByDone(final boolean done) {
+                return null;
+            }
+        };
+    }
+}
