036977fc9a1ed28d381c1b4b210499492a8e472e
damiankadziela81 <damiankadziela02@gmail.com>
2023-03-06 11:15:37 +0100

    Added new endpoint in TaskController, optimized mappings

--- src/main/java/com/example/controller/TaskController.java
+++ src/main/java/com/example/controller/TaskController.java
@@ -14,6 +14,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
+@RequestMapping("/tasks")
 class TaskController {
     public static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
@@ -22,26 +23,33 @@ class TaskController {
         this.repository = repository;
     }
 
-    @GetMapping(value = "/tasks", params = {"!sort","!page","!size"})
+    @GetMapping(params = {"!sort","!page","!size"})
     ResponseEntity<List<Task>> readAllTasks(){
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
     }
 
-    @GetMapping("/tasks")
+    @GetMapping
     ResponseEntity<List<Task>> readAllTasks(Pageable page){
         logger.info("Custom pageable");
         return ResponseEntity.ok(repository.findAll(page).getContent());
     }
 
-    @GetMapping("/tasks/{id}")
+    @GetMapping("/{id}")
     ResponseEntity<Task> getTaskByID(@PathVariable int id) {
         return repository.findById(id)
                 .map(task -> ResponseEntity.ok(task))
                 .orElseGet(()->ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/tasks")
+    @GetMapping("/search/done")
+    ResponseEntity<List<Task>> readDoneTasks(@RequestParam(defaultValue = "true") boolean state) {
+        return ResponseEntity.ok(
+                repository.findByDone(state)
+        );
+    }
+
+    @PostMapping
     ResponseEntity<Task> createTask(@RequestBody @Valid Task toCreate){
         Task createdTask = repository.save(toCreate);
         logger.info("New task created with id: " + createdTask.getId());
@@ -49,7 +57,7 @@ class TaskController {
     }
 
     @Transactional
-    @PutMapping("/tasks/{id}")
+    @PutMapping("/{id}")
     ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate) {
         if(!repository.existsById(id)) {
             return ResponseEntity.notFound().build();
@@ -63,7 +71,7 @@ class TaskController {
     }
 
     @Transactional
-    @PatchMapping("/tasks/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<?> toggleTask(@PathVariable int id) {
         if(!repository.existsById(id)) {
             return ResponseEntity.notFound().build();
