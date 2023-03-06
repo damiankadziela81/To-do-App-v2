a97942e66d2c261f871d2226d046541fa7a98a91
damiankadziela81 <damiankadziela02@gmail.com>
2023-03-06 15:47:05 +0100

    Added ExceptionHandlers for exceptions that may be thrown in services

--- src/main/java/com/example/controller/TaskGroupController.java
+++ src/main/java/com/example/controller/TaskGroupController.java
@@ -50,4 +50,15 @@ class TaskGroupController {
         taskGroupService.toggleGroup(id);
         return ResponseEntity.noContent().build();
     }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    ResponseEntity<?> handleIllegalArgument(IllegalArgumentException e) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    ResponseEntity<String> handleIllegalState(IllegalStateException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
 }
