23dc33992f5feb088517e0f0d433db72f15bf030
damiankadziela81 <damiankadziela02@gmail.com>
2023-03-07 13:14:59 +0100

    Added the ability to add the project in the form

--- src/main/java/com/example/controller/ProjectController.java
+++ src/main/java/com/example/controller/ProjectController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import com.example.logic.ProjectService;
 import com.example.model.ProjectStep;
 import com.example.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
@@ -12,9 +13,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/projects")
 class ProjectController {
+    private final ProjectService service;
+
+    ProjectController(final ProjectService service) {
+        this.service = service;
+    }
+
     @GetMapping
     String showProjects(Model model) {
         model.addAttribute("project", new ProjectWriteModel());
+        return "projects";
+    }
+
+    @PostMapping
+    String addProject(@ModelAttribute("project") ProjectWriteModel current, Model model) {
+        service.create(current);
+        model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("message", "Projekt został dodany");
         return "projects";
     }
 
