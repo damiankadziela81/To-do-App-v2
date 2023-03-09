9d86a833e10b55576ed58691f3969004d97b06b7
damiankadziela81 <damiankadziela02@gmail.com>
2023-03-09 16:39:47 +0100

    Added TaskEvent abstract class with 2 child classes for reporting when task changed it's done state. Adjusted Task and TaskController.

---
+++ src/main/java/com/example/model/event/TaskEvent.java
@@ -0,0 +1,35 @@
+package com.example.model.event;
+
+import com.example.model.Task;
+
+import java.time.Clock;
+import java.time.Instant;
+
+public abstract class TaskEvent {
+    public static TaskEvent changed(Task source) {
+        return source.isDone() ? new TaskDone(source) : new TaskPending(source);
+    }
+    private int taskId;
+    private Instant occurrence;
+
+    TaskEvent(final int taskId, Clock clock) {
+        this.taskId = taskId;
+        this.occurrence = Instant.now(clock);
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public Instant getOccurrence() {
+        return occurrence;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "{" +
+                "taskId=" + taskId +
+                ", occurrence=" + occurrence +
+                '}';
+    }
+}
