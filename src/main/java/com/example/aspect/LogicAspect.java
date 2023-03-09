a104ba62483e2e194dbe2b20e9a0c3359b23aeaf
damiankadziela81 <damiankadziela02@gmail.com>
2023-03-09 14:32:01 +0100

    Added new logger using @Before annotation on createGroup method

--- src/main/java/com/example/aspect/LogicAspect.java
+++ src/main/java/com/example/aspect/LogicAspect.java
@@ -2,21 +2,36 @@ package com.example.aspect;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 class LogicAspect {
+    public static final Logger logger = LoggerFactory.getLogger(LogicAspect.class);
     private final Timer projectCreateGroupTimer;
 
     LogicAspect(final MeterRegistry registry) {
         projectCreateGroupTimer = registry.timer("logic.project.create.group");
     }
 
-    @Around("execution(* com.example.logic.ProjectService.createGroup(..))")
+    @Pointcut("execution(* com.example.logic.ProjectService.createGroup(..))")
+    static void projectServiceCreateGroup() {
+    }
+
+    @Before("projectServiceCreateGroup()")
+    void logMethodCall(JoinPoint jp) {
+        logger.info("Before {} with {}", jp.getSignature().getName(), jp.getArgs());
+    }
+
+    @Around("projectServiceCreateGroup()")
     Object aroundProjectCreateGroup(ProceedingJoinPoint jp) {
         return projectCreateGroupTimer.record(()-> {
             try {
